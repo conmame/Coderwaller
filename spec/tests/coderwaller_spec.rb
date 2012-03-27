141befd797386000de02bbd87105cc5ca76fbc94
con_mame <con_mame@conma.me>
2012-03-27 19:50:50 +0900

    version 0.0.2

--- spec/tests/coderwaller_spec.rb
+++ spec/tests/coderwaller_spec.rb
@@ -4,5 +4,5 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 include CoderWaller
 
 describe Coderwaller do
-  it { Coderwaller::VERSION.should eq '0.0.1' }
+  it { Coderwaller::VERSION.should eq '0.0.2' }
 end
